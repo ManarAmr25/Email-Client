3ff5782a372fdedd17006150e4a54724bce5e1f5
nourwalid <58997274+nourwalid70@users.noreply.github.com>
2020-12-13 23:48:22 +0200

    Add files via upload

---
+++ Mail_front/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <component v-bind:is="main_window"
+</template>
+
+<script>
+import main_window from './components/main_window.vue'
+
+export default {
+  name: 'App',
+  components: {
+    main_window
+  }
+}
+</script>>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Mail_front/src/components/main_window.vue
@@ -0,0 +1,20 @@
+<template>
+  <button onclick="window.location.href='https://www.youtube.com/';" class="zr">Sign in</button>
+  <button onclick="window.location.href='https://www.youtube.com/';" class="zr">Sign in</button>
+</template>
+
+<script>
+export default {
+  name: "main_window"
+}
+</script>
+
+<style scoped>
+body{
+  background-image:url('../assets/logo-icon.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size:50% 100%;
+  background-position:right;
+}
+</style>
