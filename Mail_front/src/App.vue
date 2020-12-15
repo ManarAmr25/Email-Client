b2c516b6bd519f32e76f56c969f03c90adb10297
nourwalid <58997274+nourwalid70@users.noreply.github.com>
2020-12-16 01:10:01 +0200

    Add files via upload

--- Mail_front/src/App.vue
+++ Mail_front/src/App.vue
@@ -2,34 +2,62 @@
   <main_window v-if="show1"></main_window>
   <sign_in v-if="show2"></sign_in>
   <sign_up v-if="show3"></sign_up>
+  <site @submit="setCompenent(4)" v-if="show4"></site>
 </template>
 
 <script>
 import main_window from './components/main_window.vue';
 import sign_in from "@/components/sign_in";
 import sign_up from "@/components/sign_up";
+import site from "@/components/site";
 
 export default {
   name: 'App',
   components: {
+    site,
     main_window,
     sign_in,
     sign_up
+  },
+  data(){
+    return({
+        shown: 3,
+    })
   },
   methods:{
     myFunction(){
       window.location.href = "/components/sign_up.vue";
     },
+    setCompenent(num){
+      this.shown = num;
+      console.log("component set");
+    }
   },
   computed:{
 
     show1(){
+        if(this.shown == 1){
+            return true;
+        }
       return false;
-    }, show2(){
+    },
+    show2(){
+      if(this.shown == 2){
+        return true;
+      }
       return false;
     },
     show3(){
-      return true;
+      if(this.shown == 3) {
+        return true;
+      }
+      return false;
+    },
+    show4(){
+      if(this.shown == 4) {
+        return true;
+      }
+      return false;
     },
   }
 }
