e99ce7586e5c567a09859c0a2ed1fb4d807b1c0c
nada-hassan <60989613+nada-hassan@users.noreply.github.com>
2020-12-20 01:40:55 +0200

    Delete AttachmentsSort.java

--- backend/src/main/java/com/example/emailclient/Email/AttachmentsSort.java
+++
@@ -1,9 +0,0 @@
-package com.example.emailclient.Email;
-
-public class AttachmentsSort implements ISortStrategy {
-    @Override
-    public Email[] Sort(Email[] m) {
-
-        return null;
-    }
-}
