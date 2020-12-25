ed283a021efbed42b17c84361f6a34157c98752b
nada-hassan <60989613+nada-hassan@users.noreply.github.com>
2020-12-25 14:55:01 +0200

    Add files via upload

--- backend/src/main/java/com/example/emailclient/user/Contact.java
+++ backend/src/main/java/com/example/emailclient/user/Contact.java
@@ -3,8 +3,8 @@ package com.example.emailclient.user;
 import java.util.ArrayList;
 
 public class Contact {
-     String name;
-     ArrayList<String> emails;
+    public String name;
+    public ArrayList<String> emails;
 
     public Contact(String name,ArrayList<String> emails){
         this.name=name;
